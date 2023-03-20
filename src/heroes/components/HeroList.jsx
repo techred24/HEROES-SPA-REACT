28ad26fc1d59e5e2c72bb0e1325cc4dc2d7f9ad1
Rafael <f.rafael.temich24@gmail.com>
2023-03-20 11:05:48 -0600

    Component to show heroes created

--- src/heroes/components/HeroList.jsx
+++ src/heroes/components/HeroList.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
+import { HeroCard } from './';
 import { getHeroesByPublisher } from '../helpers'
 
 export const HeroList = ({ publisher }) => {
     const heroes = getHeroesByPublisher( publisher );
   return (
-    heroes.map(heroe => (
-        <li key={heroe.id}>
-            {heroe.superhero}
-        </li>
-    ))
+    <div className='row rows-cols-1 row-cols-md-3 g-3'>
+      {
+        heroes.map(heroe => (
+          <HeroCard key={heroe.id} {...heroe}/>
+      ))
+      }
+    </div>
   )
 }
