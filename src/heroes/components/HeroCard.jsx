77ca9cc1d9670b7374a24825e2c741e8f79ea6ee
Rafael <f.rafael.temich24@gmail.com>
2023-03-21 11:24:26 -0600

    Creating a new component to show characteres that interpreted the role

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -1,4 +1,10 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+
+const CharactersByHero = ({ alter_ego, characters}) => {
+    // return (alter_ego !== characters) && (<p>{characters}</p>)
+    return (alter_ego !== characters) ? (<p>{characters}</p>) : (<></>);
+}
 
 export const HeroCard = ({
     id,
@@ -22,8 +28,16 @@ export const HeroCard = ({
                     <div className="card-body">
                         <h5 className="card-title">{ superhero }</h5>
                         <p className="cart-text">{ alter_ego }</p>
+                        {
+                            <CharactersByHero alter_ego={alter_ego} characters={characters} />
+                        }
+                        <p className='card-text'>
+                            <small className='text-muted'>{first_appearance}</small>
+                        </p>
 
-                        <p>{ characters }</p>
+                        <Link to={`/hero/${ id }`}>
+                            Más...
+                        </Link>
                     </div>
                 </div>
             </div>
