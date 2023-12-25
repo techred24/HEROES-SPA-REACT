8213ab726d23e4c7a0e3427b3ab877bf2f68d0c1
TeChRF24 <f.rafael.temich24@gmail.com>
2023-12-25 14:55:27 -0600

    Modifying HeroPage and applying useMemo

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,13 +1,34 @@
-import React from 'react'
-import { Navigate, useParams } from 'react-router-dom'
+import React, { useMemo } from 'react'
+import { Navigate, useParams, useNavigate } from 'react-router-dom'
 import { getHeroById } from '../helpers';
 
 export const HeroPage = () => {
   const { id } = useParams();
-  const hero = getHeroById(id);
+  // useMemo will execute function getHeroById only if id changes: [id]
+  const hero = useMemo(() => getHeroById(id), [id]);
+  const navigate = useNavigate();
+  const onNavigateBack = () => {
+    navigate(-1);
+  }
   //console.log(hero)
   if (!hero) return <Navigate to={'/marvel'}/>
   return (
-    <h1>{hero.superhero}</h1>
+    <div className='row mt-5'>
+      <div className="col-4">
+        <img src={`/assets/heroes/${id}.jpg`} alt={hero.superhero} className='img-thumbnail'/>
+      </div>
+      <div className="col-8">
+        <h3>{ hero.superhero }</h3>
+        <ul className='list-group list-group-flush'>
+          <li className='list-group-item'><b>Alter ego:</b> {hero.alter_ego} </li>
+          <li className='list-group-item'><b>Publisher:</b> {hero.publisher} </li>
+          <li className='list-group-item'><b>First appearance:</b> {hero.first_appearance} </li>
+        </ul>
+
+        <h5 className='mt-3'>Characters</h5>
+        <p> {hero.characters} </p>
+        <button className='btn btn-outline-primary' onClick={onNavigateBack}>Regresar</button>
+      </div>
+    </div>
   )
 }
