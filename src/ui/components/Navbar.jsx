c58fbb92ddf26db8285c9e92c9db8d9bb3eacbc9
Rafael Temich <f.rafael.temich24@gmail.com>
2023-12-27 12:49:41 -0600

    Implementing SearchPage

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -34,6 +34,13 @@ export const Navbar = () => {
                     >
                         DC
                     </NavLink>
+
+                    <NavLink 
+                        className={ ({isActive}) => `nav-item nav-link ${isActive ? 'active' : '' }`}
+                        to="/search"
+                    >
+                        Search
+                    </NavLink>
                 </div>
             </div>
 
