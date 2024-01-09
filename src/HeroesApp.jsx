b83ee7f4cfcc23361548abc3a454bef916390509
Rafael Temich <f.rafael.temich24@gmail.com>
2024-01-09 11:50:35 -0600

    Implementing provider

--- src/HeroesApp.jsx
+++ src/HeroesApp.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { AppRouter } from './router/AppRouter'
+import { AuthProvider } from './auth'
 
 export const HeroesApp = () => {
   return (
     <>
-    <AppRouter/>
+    <AuthProvider>
+      <AppRouter/>
+    </AuthProvider>
     </>
   )
 }
