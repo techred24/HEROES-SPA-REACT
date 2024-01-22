b58fa6e1355ad5682cd0639d8c98b08c1e4c6168
Rafael Temich <f.rafael.temich24@gmail.com>
2024-01-22 13:39:21 -0600

    Getting user name after logging to show it in navbar component

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,7 +1,11 @@
+import { useContext } from 'react';
 import { Link, NavLink, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../auth/context/AuthContext';
 
 
 export const Navbar = () => {
+    // const { user } = useContext(AuthContext);
+    const { user } = useContext(AuthContext);
     const navigate = useNavigate();
     const onLogout = () => {
         navigate('/login', {
@@ -47,7 +51,7 @@ export const Navbar = () => {
             <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
                 <ul className="navbar-nav ml-auto">
                     <span className='nav-item nav-link text-primary'>
-                        Erik El Rojo
+                        { user?.name ?? 'Leif Erikson' }
                     </span>
                     <button onClick={onLogout} className='nav-item nav-link btn'>
                         Logout
